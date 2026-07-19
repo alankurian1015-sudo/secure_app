2b0c4d6c36109dd6866478a603e99400e447c9b2
alankurian1015-sudo <alankurian1015-sudo@users.noreply.github.com>
2026-07-19 14:16:47 +0530

    delete from new

--- main.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-
-if __name__ == '__main__':
-    with app.app_context():
-        from models import User
-        import routes
-        db.create_all()
-    app.run(debug=True)
